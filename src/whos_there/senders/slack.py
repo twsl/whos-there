19b8af3b5bc4df8f3d85825accf4f8313f895259
Luis Martí <lmarti@users.noreply.github.com>
2024-08-13 12:10:13 -0400

    allowing to configure sender username  and emoji, removin [] when there are no user mentiosn

--- src/whos_there/senders/slack.py
+++ src/whos_there/senders/slack.py
@@ -6,25 +6,37 @@ from whos_there.senders.base import Sender
 
 
 class SlackSender(Sender):
-    def __init__(self, webhook_url: str, channel: str, user_mentions: list[str] | None = None) -> None:
+    def __init__(
+        self,
+        webhook_url: str,
+        channel: str,
+        user_mentions: list[str] | None = None,
+        sender_username: str = "Who's There notification",
+        icon_emoji: str = ":bell:",
+    ) -> None:
         """Initialize the Slack sender.
 
         Args:
-            webhook_url: The Slack webhook URL.
-            channel: The Slack channel name.
-            user_mentions: The list of users to mention.
+            webhook_url (str): The Slack webhook URL.
+            channel (str): The Slack channel name.
+            sender_username (str): User name used for notifications (defaults to "Who's There notification").
+            user_mentions (List[str]): The list of users to mention (defaults to None).
+            icon_emoji (str): Emoji to use for the notifications (defaults to ":bell:")
+                              See <https://www.webfx.com/tools/emoji-cheat-sheet/> for options.
         """
         super().__init__()
         self.webhook_url = webhook_url
         self.channel = channel
-        self.user_mentions = " ".join(user_mentions) if user_mentions else []
+        self.sender_username = sender_username
+        self.icon_emoji: str = icon_emoji
+        self.user_mentions = " ".join(user_mentions) if user_mentions else ""
 
     def send(self, text: str) -> Any:
         data = {
-            "username": "Knock Knock",
+            "username": self.sender_username,
             "channel": self.channel,
             "link_names": 1,
-            "icon_emoji": ":clapper:",  # ":tada:"
+            "icon_emoji": self.icon_emoji,
             "text": f"{text} {self.user_mentions}",
         }
         return self._send_json(self.webhook_url, data)

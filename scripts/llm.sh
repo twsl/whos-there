123037494c5966be4d64b9726f509775b14416ba
twsl <45483159+twsl@users.noreply.github.com>
2026-07-27 03:11:58 +0000

    chore: update files via copier

--- scripts/llm.sh
+++ scripts/llm.sh
@@ -4,6 +4,7 @@ curl -fsSL https://gh.io/copilot-install | bash
 curl -fsSL https://claude.ai/install.sh | bash
 curl -fsSL https://chatgpt.com/codex/install.sh | CODEX_NON_INTERACTIVE=1 bash
 
+export PATH="$HOME/.local/bin:$PATH"
 curl -fsSL https://raw.githubusercontent.com/rtk-ai/rtk/refs/heads/master/install.sh | bash
 
 curl -fsSL https://raw.githubusercontent.com/colbymchenry/codegraph/main/install.sh | bash
